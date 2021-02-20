3c6a521c4ee6af75a638249458dd56c70b9cbd40
SensorVN-Code <69109773+SensorVN-Code@users.noreply.github.com>
2021-02-20 19:54:31 +0700

    Create test_TTP229_ESP8266_2-7-4.ino

---
+++ test_TTP229_ESP8266_2-7-4.ino
@@ -0,0 +1,56 @@
+/*
+  Created by SENSORVN.COM, February 19, 2021.
+  Wiring
+  VCC -> 3.3V
+  GND -> GND
+  SCL -> GPIO3
+  SDO -> GPIO4
+*/
+const int SDO = 4; //SDO
+const int SCL_ = 3;//SCL
+const int LED = 14;//LED
+byte data_ = 0;
+byte data = 0;
+void ICACHE_RAM_ATTR handleInterrupt();
+
+/* Function */
+void work()
+{
+  for(byte i=1;i<17;i++)
+  { 
+    data_ = i;
+    attachInterrupt(digitalPinToInterrupt(SDO), handleInterrupt, FALLING);
+    digitalWrite(SCL_,LOW);
+    delay(1);
+    digitalWrite(SCL_,HIGH);
+    delay(1);
+  }
+  delay(2);
+  detachInterrupt(digitalPinToInterrupt(SDO));
+  delay(1000);
+}
+
+/* Arduino Function */
+void setup() 
+{ 
+  Serial.begin(115200);
+  pinMode(LED,OUTPUT); 
+  pinMode(SCL_,OUTPUT); 
+  pinMode(SDO, INPUT); 
+  delay(1000);
+  Serial.println("Calibrating Sensor .................. ");
+  delay(4000);//calibration
+  digitalWrite(LED,HIGH);
+  Serial.print("Done");
+} 
+
+void loop() 
+{ 
+  work();
+} 
+
+void ICACHE_RAM_ATTR handleInterrupt() 
+{ 
+  data = data_;
+  Serial.println(data); 
+}
