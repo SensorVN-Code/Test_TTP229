48ed2595d9c7b9699dba0aa334cb743bd9fee66b
SensorVN-Code <69109773+SensorVN-Code@users.noreply.github.com>
2021-02-20 19:26:02 +0700

    Update test_TTP229_ESP8266_2-7-4.ino

--- test_TTP229_ESP8266_2-7-4.ino
+++ test_TTP229_ESP8266_2-7-4.ino
@@ -37,8 +37,10 @@ void setup()
   pinMode(LED,OUTPUT); 
   pinMode(SCL_,OUTPUT); 
   pinMode(SDO, INPUT); 
+  println("Calibrating Sensor .................. );
   delay(5000);//calibration
   digitalWrite(LED,HIGH);
+  print("Done");
 } 
 
 void loop() 
