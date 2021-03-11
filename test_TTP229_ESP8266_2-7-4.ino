f61bdd6431f34e431e193dde0d6b212c8d985e4a
SensorVN-Code <69109773+SensorVN-Code@users.noreply.github.com>
2021-03-11 16:06:45 +0700

    Update test_TTP229_ESP8266_2-7-4.ino

--- test_TTP229_ESP8266_2-7-4.ino
+++ test_TTP229_ESP8266_2-7-4.ino
@@ -9,25 +9,46 @@
 const int SDO = 4; //SDO
 const int SCL_ = 3;//SCL
 const int LED = 14;//LED
+volatile int flag = 0;
+volatile int flag2 = 0;
 byte data_ = 0;
 byte data = 0;
 void ICACHE_RAM_ATTR handleInterrupt();
 
 /* Function */
+void delaySensor()
+{
+  for(int i=0;i<50;i++)
+  {
+    if(!digitalRead(SDO))
+    {
+      if(flag2 == 0)
+      {
+        data = data_;
+        flag2 ++;
+      }
+      if(flag2 == 1)
+      {
+        Serial.println(data);
+        flag2 ++;
+      }
+    }
+  }
+}
 void work()
 {
-  for(byte i=1;i<17;i++)
-  { 
+  for(int i = 1;i<17;i++)
+  {
     data_ = i;
-    attachInterrupt(digitalPinToInterrupt(SDO), handleInterrupt, FALLING);
     digitalWrite(SCL_,LOW);
-    delay(1);
+    delaySensor();
     digitalWrite(SCL_,HIGH);
-    delay(1);
+    delaySensor();
   }
-  delay(2);
-  detachInterrupt(digitalPinToInterrupt(SDO));
-  delay(1000);
+  flag2 = 0;
+  flag = 0;
+  delay(300);
+  attachInterrupt(digitalPinToInterrupt(SDO), handleInterrupt, FALLING);
 }
 
 /* Arduino Function */
@@ -36,21 +57,25 @@ void setup()
   Serial.begin(115200);
   pinMode(LED,OUTPUT); 
   pinMode(SCL_,OUTPUT); 
-  pinMode(SDO, INPUT); 
-  delay(1000);
+  pinMode(SDO, INPUT);
+  digitalWrite(SCL_,HIGH); 
   Serial.println("Calibrating Sensor .................. ");
   delay(4000);//calibration
+  attachInterrupt(digitalPinToInterrupt(SDO), handleInterrupt, FALLING);  
   digitalWrite(LED,HIGH);
   Serial.print("Done");
 } 
 
 void loop() 
 { 
-  work();
+  if(flag == 1)
+  {
+    work();
+  }
 } 
 
 void ICACHE_RAM_ATTR handleInterrupt() 
 { 
-  data = data_;
-  Serial.println(data); 
+  flag = 1;
+  detachInterrupt(digitalPinToInterrupt(SDO));
 }
